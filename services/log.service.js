a1505c0d31a9074fabd058604f8bba37e024ad96
GlebRaychenkov <raychenkovgleb@gmail.com>
2021-12-16 04:59:31 +0300

    created save function weather-data.json in homedir

--- services/log.service.js
+++ services/log.service.js
@@ -1,19 +1,20 @@
 import chalk from 'chalk';
 import dedent from 'dedent-js';
+const { bgRed, bgGreen, bgCyan } = chalk;
 
 export const printError = (error) => {
-	console.log(` ${chalk.bgRed(' Error ')}: ${error}`);
+	console.log(` ${bgRed(' Error ')}: ${error}`);
 };
 
 export const printSuccess = (message) => {
-	console.log(` ${chalk.bgGreen(' Success ')}: ${error}`);
+	console.log(` ${bgGreen(' Success ')}: ${message}`);
 };
 
 export const printHelp = () => {
-	console.log(dedent`${chalk.bgCyan(' Help ')}: 
+	console.log(dedent`${bgCyan(' Help ')}: 
 	without parametrs - show weather
-	-s [CITY] - select city
 	-h - show help
+	-s [CITY] - select city
 	-t [API_KEY] - save token
 	`);
 };
--- weather.js
+++ weather.js
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { printHelp } from './services/log.service.js';
+import { printHelp, printSuccess, printError } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+	try {
+		await saveKeyValue('token', token);
+		printSuccess(`token ${token} is saved`);
+	} catch (e) {
+		console.log(e);
+		printError(e.message);
+	}
+};
 
 const initCLI = () => {
 	let res = getArgs(process.argv);
@@ -11,7 +22,7 @@ const initCLI = () => {
 
 	}
 	if (res.t) {
-
+		return saveToken(res.t);
 	}
 };
 
